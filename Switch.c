642a381d0f2085002efa425b779fb693c04785c0
alpshams <alpshams@gmail.com>
2026-04-13 11:49:54 -0500

    DPAD interfacting modification (4-way implementation)

--- Switch.c
+++ Switch.c
@@ -2,21 +2,20 @@
  * Switch.c
  *
  *  Created on: January 12, 2026
- *      Author:
+ *      Author: Michael McElroy & Alp Shamsapour
  */
 #include <ti/devices/msp/msp.h>
 #include "../inc/LaunchPad.h"
 // LaunchPad.h defines all the indices into the PINCM table
 void Switch_Init(void){
+     IOMUX->SECCFG.PINCM[PB1INDEX] = 0x00050081;
      IOMUX->SECCFG.PINCM[PB2INDEX] = 0x00050081;
      IOMUX->SECCFG.PINCM[PB3INDEX] = 0x00050081;
- 
+     IOMUX->SECCFG.PINCM[PB4INDEX] = 0x00050081;
 }
 // return current state of switches
 uint32_t Switch_In(void){
     uint32_t keys;
-    keys = ((GPIOB->DIN31_0 >> 2) & 0b11);
-     
-
+    keys = ((GPIOB->DIN31_0 >> 1) & 0b1111);
     return keys; // replace this line
 }
